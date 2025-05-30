a4165ab0fad807c4d9c12ec2f33d8bb0b9db414e
Djordje3002 <djkartaljevic@gmail.com>
2025-05-30 20:52:26 +0200

    Initial Commit

---
+++ GithubConnecting/GithubConnectingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubConnectingApp.swift
+//  GithubConnecting
+//
+//  Created by Djordje on 30. 5. 2025..
+//
+
+import SwiftUI
+
+@main
+struct GithubConnectingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubConnectingTests/GithubConnectingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubConnectingTests.swift
+//  GithubConnectingTests
+//
+//  Created by Djordje on 30. 5. 2025..
+//
+
+import Testing
+@testable import GithubConnecting
+
+struct GithubConnectingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
